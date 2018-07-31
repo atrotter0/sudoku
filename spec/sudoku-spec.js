2770bba7aefaf412d1563aafcfd2ff9996af843d
Abel Trotter, Kelli Brooke and Nate Cottle <nate@>
2018-07-31 14:30:25 -0700

    Add buildColumns function, validate numbers in columns and rows.

--- spec/sudoku-spec.js
+++ spec/sudoku-spec.js
@@ -7,12 +7,17 @@ describe('Sudoku', function() {
     sudoku = new Sudoku();
   });
 
-  it('should build game grid', function() {
-    expect(sudoku.masterArray.length).toEqual(sudoku.MAX_ROWS);
+  it('should build game rows', function() {
+    expect(sudoku.rowsArray.length).toEqual(sudoku.MAX_ROWS);
   });
 
-  it('should check if all rows are unique', function(){
-    expect(sudoku.checkRows()).toEqual(true)
+  it('should build game columns', function() {
+    expect(sudoku.columnsArray.length).toEqual(sudoku.MAX_ROWS);
+  });
+
+  it('should check if all rows and columns only include 1-9', function(){
+    expect(sudoku.validateNumbers(sudoku.rowsArray)).toEqual(true);
+    expect(sudoku.validateNumbers(sudoku.columnsArray)).toEqual(true);
   });
 
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,36 +1,45 @@
 export function Sudoku() {
   this.MAX_ROWS = 9;
+  this.key = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   this.masterArray = [];
-  this.buildKey();
+  this.rowsArray = [];
+  this.columnsArray = [];
+  this.buildRows();
+  this.buildColumns();
 }
 
-Sudoku.prototype.buildKey = function() {
+
+Sudoku.prototype.buildRows = function() {
   var initialArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  this.masterArray.push(initialArray);
+  this.rowsArray.push(initialArray);
   var newArray = initialArray.map(x => x);
   for(var i = 1; i < this.MAX_ROWS; i++) {
     var firstItem = newArray.shift();
     newArray.push(firstItem);
     var newerArray = newArray.map(x => x);
-    this.masterArray.push(newerArray);
+    this.rowsArray.push(newerArray);
+  }
+  this.masterArray.push(this.rowsArray);
+};
+
+Sudoku.prototype.buildColumns = function () {
+  for(var x = 0; x < this.rowsArray.length; x++) {
+    var newColumnArray = [];
+    for(var y = 0; y < this.rowsArray[x].length; y++) {
+      newColumnArray.push(this.rowsArray[y][x]);
+    }
+    this.columnsArray.push(newColumnArray.map(x => x));
   }
 };
 
-Sudoku.prototype.checkRows = function() {
-  var check = true;
-  for(x = 0; x < this.masterArray.length; x++) {
-    if (check === true) {
-      for(y = 0; y < this.masterArray.length; y++) {
-        if (x !== y) {
-          for(z = 0; z <this.masterArray[x].length; z++) {
-            if (this.masterArray[x][z] == this.masterArray[y][z]) {
-              check = false;
-            }
-          }
-        }
+Sudoku.prototype.validateNumbers = function (arrays) {
+  var valid = true;
+  for(var i = 0; i < arrays.length; i++) {
+    for(var j = 0; j < this.MAX_ROWS; j++) {
+      if (!this.key.includes(arrays[i][j])) {
+        valid = false;
       }
-    } else {
-
     }
   }
+  return valid;
 };
